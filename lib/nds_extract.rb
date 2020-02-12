3fe23ce7defed896e75a3267e2a6a902c312d949
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-12 15:05:17 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-
+pp directors_database
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
@@ -18,5 +18,19 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  producer_i = 0 
+  while producer_i < nds.count do 
+  movies_i = 0 
+  producer_count = 0 
+  producer_name = nds[producer_i][:name]
+    #go into movies: worldwide_gross:
+    while movies_i < nds[producer_i][:movies].count do 
+    producer_count += nds[producer_i][:movies][movies_i][:worldwide_gross] 
+    #name of director / movies
+    movies_i += 1 
+    end
+  producer_i += 1
+  result[producer_name] = producer_count
+  end
+   result
 end
